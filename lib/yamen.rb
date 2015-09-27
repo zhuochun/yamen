3220abc65c6b09984c08c378de6577ace9ac2c62
Zhuochun <zhuochun@hotmail.com>
2015-09-28 00:46:01 +0800

    basic parser

--- lib/yamen.rb
+++ lib/yamen.rb
@@ -1,5 +1,127 @@
 require "yamen/version"
 
 module Yamen
-  # Your code goes here...
+  class Governor
+    attr_reader :junction_module, :junction_rules, :user_module, :user_rules
+
+    def initialize(junction_rule_module, user_rule_module)
+      @junction_module = junction_rule_module
+      @junction_rules = junction_rule_module.constants
+      @user_module = user_rule_module
+      @user_rules = user_rule_module.constants
+    end
+
+    # TODO
+    def user_rules_config
+      @user_rules.map do |rule|
+        {
+          type: rule,
+          args: @user_module.const_get(rule).requirements
+        }
+      end
+    end
+
+    def parser
+      @parser ||= Parser.new(@junction_module, @user_module)
+    end
+  end
+
+  class Parser
+    def initialize(junction_rule_module, user_rule_module)
+      @junction_module = junction_rule_module
+      @junction_rules = junction_rule_module.constants
+      @user_module = user_rule_module
+      @user_rules = user_rule_module.constants
+    end
+
+    # read a JSON hash, and return the root Rule object
+    def read(json_rule)
+      read_rule(json_rule)
+    end
+
+    # validate a JSON hash, and return errors or nil
+    def validate(json_rule)
+      errors = []
+      validate_rule(json_rule, errors)
+      errors
+    end
+
+    private
+
+    def validate_rule(rule, errors)
+      # TODO implement
+    end
+
+    def read_rule(rule)
+      type = rule['type'].to_sym # TODO .capitalize.to_sym
+
+      if @junction_rules.include?(type)
+        rule_class = @junction_module.const_get(type)
+        conditions = rule['args'].map { |arg| read_rule(arg) }
+        rule_class.new(conditions)
+      elsif @user_rules.include?(type)
+        rule_class = @user_module.const_get(type)
+        rule_class.new(rule['args'])
+      else
+        raise ArgumentError.new("Invalid Rule: #{type}")
+      end
+    end
+  end
+
+  class BaseRule
+  end
+
+  module BooleanRule
+    class And
+      def initialize(conditions)
+        @conditions = conditions
+      end
+
+      def eval(facts)
+        # TODO record failed condition name and facts
+        @conditions.all? { |condition| condition.eval(facts) == true }
+      end
+
+      # TODO better to_s for debugging
+    end
+
+    class Or
+      def initialize(conditions)
+        @conditions = conditions
+      end
+
+      def eval(facts)
+        # TODO record failed condition name and facts
+        @conditions.any? { |condition| condition.eval(facts) == true }
+      end
+
+      # TODO better to_s for debugging
+    end
+  end
+
+  module MathRule
+    class Plus
+      def initialize(conditions)
+        @conditions = conditions
+      end
+
+      def eval(facts)
+        @conditions.all? { |condition| condition.eval(facts) == true }
+      end
+
+      # TODO better to_s for debugging
+    end
+
+    class Minus
+      def initialize(conditions)
+        @conditions = conditions
+      end
+
+      def eval(facts)
+        @conditions.any? { |condition| condition.eval(facts) == true }
+      end
+
+      # TODO better to_s for debugging
+    end
+  end
 end
--- spec/yamen_spec.rb
+++ spec/yamen_spec.rb
@@ -1,11 +1,79 @@
 require 'spec_helper'
 
-describe Yamen do
-  it 'has a version number' do
-    expect(Yamen::VERSION).not_to be nil
+module UserRule
+  class PickUpTime < Yamen::BaseRule
+    # TODO defined in BaseRule, or use ActiveRecord validates
+    # require_config :weekday, :intger, range: 0..6
+    # require_config :startTime, :string, format: /\d{2}:\d{2}/
+    # require_config :endTime, :string, format: /\d{2}:\d{2}/
+
+    def initialize(args)
+      @weekday = args.fetch('weekday')
+      @startTime = args.fetch('startTime')
+      @endTime = args.fetch('endTime')
+    end
+
+    def eval(facts)
+      facts[:booking].pick_up_time.wday == @weekday
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  class PickUpLocation < Yamen::BaseRule
+    def initialize(args)
+      @polygon = args.fetch('polygon') # TODO convert polygon
+    end
+
+    def eval(facts)
+      @polygon.cover?(facts[:booking].pick_up_latitude, facts[:booking].pick_up_longitude)
+    end
+  end
+end
+
+describe Yamen do
+  describe Yamen::Governor do
+    it 'return all the rules' do
+      governor = Yamen::Governor.new(Yamen::BooleanRule, UserRule)
+      expect(governor.junction_rules).to match_array([:And, :Or])
+      expect(governor.user_rules).to match_array([:PickUpTime, :PickUpLocation])
+    end
+
+    it 'return all the rules in details' do
+      # TODO pending implement
+    end
+  end
+
+  describe Yamen::Parser do
+    it 'parse and correctly' do
+      parser = Yamen::Parser.new(Yamen::BooleanRule, TrueClass)
+      root = parser.read({ 'type' => 'And', 'args' => [] })
+      expect(root).to be_an_instance_of(Yamen::BooleanRule::And)
+      expect(root.eval('anything')).to be(true)
+    end
+
+    it 'parse or correctly' do
+      parser = Yamen::Parser.new(Yamen::BooleanRule, TrueClass)
+      root = parser.read({ 'type' => 'Or', 'args' => [] })
+      expect(root).to be_an_instance_of(Yamen::BooleanRule::Or)
+      expect(root.eval('anything')).to be(false)
+    end
+
+    it 'parse or + pickUpTime correctly' do
+      parser = Yamen::Parser.new(Yamen::BooleanRule, UserRule)
+      root = parser.read({
+        'type' => 'Or', 'args' => [
+          { 'type' => 'PickUpTime', 'args' => { 'weekday' => 1, 'startTime' => 0, 'endTime' => 24 } },
+          { 'type' => 'PickUpTime', 'args' => { 'weekday' => 2, 'startTime' => 0, 'endTime' => 24 } }
+        ]
+      })
+      expect(root).to be_an_instance_of(Yamen::BooleanRule::Or)
+
+      Booking = Struct.new(:pick_up_time)
+
+      facts = { booking: Booking.new(Time.parse('2015-09-24 02:49:53 +0800')) }
+      expect(root.eval(facts)).to be(false)
+
+      facts = { booking: Booking.new(Time.parse('2015-09-21 02:49:53 +0800')) }
+      expect(root.eval(facts)).to be(true)
+    end
   end
 end
